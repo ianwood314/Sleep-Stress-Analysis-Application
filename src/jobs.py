d1e805200ae622c76bc25d1f8fc3405fe8fe5d29
Thy4869 <91298368+Thy4869@users.noreply.github.com>
2022-04-26 19:12:04 -0500

    Update jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,23 @@
+import uuid, os
+from hotqueue import HotQueue
+from redis import Redis
+
+redis_ip = os.environ.get('REDIS_IP', '172.17.0.1')
+q = HotQueue("queue", host=redis_ip, port=6379, db=1)
+rd = redis.StrictRedis(host=redis_ip, port=6379, db=0)
+
 def generate_jid():
     """
     Generate a pseudo-random identifier for a job.
     """
     return str(uuid.uuid4())
 
-app = Flask(__name__)
-
-def generate_job_key(jid):
+def _generate_job_key(jid):
     """
     Generate the redis key from the job id to be used when storing, retrieving or updating
     a job in the database.
     """
     return 'job.{}'.format(jid)
-
-q = HotQueue("queue", host='172.17.0.1', port=6379, db=1)
 
 def instantiate_job(jid, status, start, end):
     """
@@ -32,53 +36,21 @@ def instantiate_job(jid, status, start, end):
             'end': end.decode('utf-8')
     }
 
-@app.route('/jobs', methods=['POST'])
-def jobs_api():
-    """
-    API route for creating a new job to do some analysis. This route accepts a JSON payload
-    describing the job to be created.
-    """
-    try:
-        job = request.get_json(force=True)
-    except Exception as e:
-        return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
-    return json.dumps(jobs.add_job(job['start'], job['end']))
-
-def save_job(job_key, job_dict):
+def _save_job(job_key, job_dict):
     """Save a job object in the Redis database."""
-    rd.hset(.......)
+    rd.hset(job_key, mapping=job_dict)
 
-def queue_job(jid):
+def _queue_job(jid):
     """Add a job to the redis queue."""
-    ....
-
-if __name__ == '__main__':
-    """
-    Main entrypoint of the API server
-    """
-    app.run(debug=True, host='0.0.0.0')
+    q.put(jid)
 
 def add_job(start, end, status="submitted"):
     """Add a job to the redis queue."""
     jid = generate_jid()
     job_dict = instantiate_job(jid, status, start, end)
-    save_job(......)
-    queue_job(......)
+    save_job(_generate_job_key(jid), job_dict)
+    queue_job(jid)
     return job_dict
-
-@<...>   # fill in
-def execute_job(jid):
-    """
-    Retrieve a job id from the task queue and execute the job.
-    Monitors the job to completion and updates the database accordingly.
-    """
-    # fill in ...
-    # the basic steps are:
-    # 1) get job id from message and update job status to indicate that the job has started
-    # 2) start the analysis job and monitor it to completion.
-    # 3) update the job status to indicate that the job has finished.
-
-rd = redis.StrictRedis(host='172.17.0.1', port=6379, db=0)
 
 def update_job_status(jid, status):
     """Update the status of job with job id `jid` to status `status`."""
@@ -87,4 +59,12 @@ def update_job_status(jid, status):
         job['status'] = status
         save_job(generate_job_key(jid), job)
     else:
+        raise Exception()def update_job_status(jid, status):
+    """Update the status of job with job id `jid` to status `status`."""
+    job = get_job_by_id(jid)
+    if job:
+        job['status'] = status
+        save_job(generate_job_key(jid), job)
+    else:
         raise Exception()
+
