dd8cc58a9889d01d506f3fb1b414b5598a375c40
Thy4869 <91298368+Thy4869@users.noreply.github.com>
2022-04-26 19:13:51 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,7 +1,16 @@
-q = HotQueue("some_queue", host="<Redis_IP>", port=6379, db=1)
+from jobs import q, update_job_status
 
-@q.worker
-def do_work(item):
-    # do something with item...
-
-do_work()
+@<...>   # fill in
+def execute_job(jid):
+    """
+    Retrieve a job id from the task queue and execute the job.
+    Monitors the job to completion and updates the database accordingly.
+    """
+    # fill in ...
+    # the basic steps are:
+    # 1) get job id from message and update job status to indicate that the job has started
+    # 2) start the analysis job and monitor it to completion.
+    # 3) update the job status to indicate that the job has finished.
+    update_job_status(jid, "in progress")
+    time.sleep(15)
+    update_job_status(jid, "complete")
