14c5613dfb552ed7de64e4aa3dae44d1e07283fd
Thy4869 <91298368+Thy4869@users.noreply.github.com>
2022-04-30 14:33:06 -0500

    Update flask_api.py

--- src/flask_api.py
+++ src/flask_api.py
@@ -1,9 +1,11 @@
 # api.py
 
-  app = Flask(__name__)
+from flask import Flask
 
-  @app.route('/jobs', methods=['POST'])
-  def jobs_api():
+app = Flask(__name__)
+
+@app.route('/jobs', methods=['POST'])
+def jobs_api():
       """
       API route for creating a new job to do some analysis. This route accepts a JSON payload
       describing the job to be created.
@@ -14,5 +16,9 @@
           return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
       return json.dumps(jobs.add_job(job['start'], job['end']))
 
-  if __name__ == '__main__':
-      app.run(debug=True, host='0.0.0.0')
+@app.route('/test', methods=['GET'])
+def hello_world():
+    return 'Hello World\n'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
