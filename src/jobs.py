73b0ddf44ca50eb876f87029e3907fa7683db172
ianwood314 <93020229+ianwood314@users.noreply.github.com>
2022-05-08 12:30:08 -0500

    Update jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -1,10 +1,10 @@
 import uuid, os
 from hotqueue import HotQueue
-import redis
+from redis import Redis
 
 redis_ip = os.environ.get('REDIS_IP')
 q = HotQueue("queue", host=redis_ip, port=6379, db=1)
-rd = redis.Redis(host=redis_ip, port=6379, db=0)
+rd = Redis(host=redis_ip, port=6379, db=0)
 
 def generate_jid():
     """
