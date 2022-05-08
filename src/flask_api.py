9da59dc4fa46d628feafee0e82d2194da456d3ff
ianwood314 <93020229+ianwood314@users.noreply.github.com>
2022-05-08 13:23:28 -0500

    Update flask_api.py

--- src/flask_api.py
+++ src/flask_api.py
@@ -24,7 +24,7 @@ def hello_world():
 @app.route('/upload-data', methods=['POST'])
 def post_dataset():
     global data
-    data = pd.read_csv('app/src/test_data.xlsx')
+    data = pd.read_csv('./src/test_data.xlsx')
     
     return data['sr'].values[0]
 
