5742c31651115af5719e80c202aaaec888da0984
ianwood314 <93020229+ianwood314@users.noreply.github.com>
2022-05-12 09:14:18 -0500

    Update worker.py

--- src/worker.py
+++ src/worker.py
@@ -1,25 +1,17 @@
-from jobs import q, update_job_status
-import hotqueue
+from hotqueue import HotQueue
+import json
 import os
+import redis
+import matplotlib.pyplot as plt
+import subprocess
+from jobs import q, rd, jdb, update_job_status, img_db
 import time
-
-redis_ip = os.environ.get('REDIS_IP')
-q = hotqueue.HotQueue("Queue", host=redis_ip, port=6379, db=1)
 
 @q.worker
 def execute_job(jid):
-    """
-    Retrieve a job id from the task queue and execute the job.
-    Monitors the job to completion and updates the database accordingly.
-    """
-    # fill in ...
-    # the basic steps are:
-    # 1) get job id from message and update job status to indicate that the job has started
-    # 2) start the analysis job and monitor it to completion.
-    # 3) update the job status to indicate that the job has finished.
-    update_job_status(jid, "in progress")
-    time.sleep(5)
-    print(jid)
-    update_job_status(jid, "complete")
+    print('executing job...')
+    update_job_status(jid, 'in progress')
+
+    jdb.hset(f'job.{jid}', 'status', 'finished')
 
 execute_job()
