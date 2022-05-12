7412d02c933404fafc10896550f1cedd83fd4777
ianwood314 <93020229+ianwood314@users.noreply.github.com>
2022-05-12 09:12:48 -0500

    Update flask_api.py

--- src/flask_api.py
+++ src/flask_api.py
@@ -1,15 +1,20 @@
 # api.py
 
 from flask import Flask, request, jsonify
+
+# ADDED THESE MODULES
+import hotqueue
+import redis
+import jobs
+import os
 import pandas as pd
-import json, statistics, 
+import json, statistics
 from datetime import datetime
 from jobs import add_job, get_job_by_id
+import matplotlib.pyplot as plt
 
+from jobs import rd, q, add_job, get_job_by_id, jdb, img_db
 app = Flask(__name__)
-redis_ip = os.environ.get('REDIS_IP')
-rd = redis.Redis(host=redis_ip, port=6379, db=0)
-jdb = redis.Redis(host=redis_ip, port=6379, db=2, decode_responses=True)
 
 def current_time():
     now = datetime.now()
@@ -17,32 +22,35 @@ def current_time():
 
 @app.route('/jobs', methods=['POST', 'GET'])
 def jobs_api():
-      """
-      API route for creating a new job to do some analysis. This route accepts a JSON pay\
-load
-      describing the job to be created.
-      """
-      if request.method == 'POST':
-          try:
-              job = request.get_json(force=True)
-          except Exception as e:
-              return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format\
-(e)})
-          return json.dumps(add_job(job['row'], job['col'], job['start'], job['end']), in\
-dent=2) + '\n'
+    """
+    API route for creating a new job to do some analysis. This route accepts a JSON payload
+    describing the job to be created.
+    """
+    if request.method == 'POST':  
+        try:
+            job = request.get_json(force=True)
+        except Exception as e:
+            return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+    
+        return json.dumps(add_job(job['column 1'], job['column 2'], job['row start'], job['row end']), indent=2) + '\n'
 
-      elif request.method == 'GET':
-          redis_dict = {}
-          for key in jdb.keys():
-              redis_dict[str(key)] = {}
-              redis_dict[str(key)]['datetime'] = jdb.hget(key, 'datetime')
-              redis_dict[str(key)]['status'] = jdb.hget(key, 'status')
-          return json.dumps(redis_dict, indent=4) + '\n' + """
+    elif request.method == 'GET':
+        redis_dict = {}
+        for key in jdb.keys():
+            redis_dict[str(key)] = {}
+            redis_dict[str(key)]['datetime'] = jdb.hget(key, 'datetime')
+            redis_dict[str(key)]['status'] = jdb.hget(key, 'status')
+        return json.dumps(redis_dict, indent=4) + '\n' + """
   To submit a job, do the following:
-  curl localhost:5034/jobs -X POST -d '{"jobtype":<job>, "variable":<key>}' -H "Content-T\
-ype: application/json"
+  curl localhost:5000/jobs -X POST -d '{"column one":<col1>, "column 2":<col2>, "row start":<rowS>, "row end":<rowE>}' -H "Content-Type: application/json"
 """
 
+@app.route('/jobs/<job_uuid>', methods=['GET'])
+def get_job_result(job_uuid: str):
+    """
+    API route for checking on the status of a submitted job
+    """
+    return json.dumps(get_job_by_id(job_uuid), indent=2) + '\n'
 
 @app.route('/test', methods=['GET'])
 def hello_world():
@@ -58,41 +66,32 @@ Info/column/<col> -- return data for specific column\n~getInfo/<col>/highest --\
 st value in specific column\n~getInfo/<row>/<col> -- return value in specific r\
 ow and column\n~getLoc/<col>/<value> -- return the postions of specific value i\
 n the dataset\n~calcVar/column -- calculate the variance of specific column dat\
-a values\n"
+a values\n~/create/<sr>/<rr>/<t>/<lm>/<bo>/<rem>/<sr1>/<hr>/<sl> -- creates a row of data and saves it to the \
+database\n~/update/<row>/<col>/<new_val> -- Updates an existing datapoint with a new val\
+ue\n~/delete -- deletes everything from the database\n~/graph/<columnOne>/<columnTwo> -- graphs two data columns and exports an image"
 
 
 @app.route('/uploadData', methods=['GET','POST'])
 def upload_dataset():
     global data
-    data = pd.read_csv('SaYoPillow-2.csv')
+    data = pd.read_csv('./src/SaYoPillow-2.csv')
     data_json = data.to_json(orient='columns')
     data_json = json.loads(data_json)
-
-    if request.method == 'GET':
-        start = request.args.get('start', 0)
-        try:
-            start = int(start)
-            for key in rd.scan_iter():
-                d = rd.hgetall(key)
-                d = {key.decode('utf-8'): d[key].decode('utf-8') for key in d.keys()}
-                data.append(d)
-        except ValueError:
-            return ' -- Invalid start parameter. Please ensure the start parameter is an \
-integer --\n'
-        return f"{type(data_json)}\n"
+    
+    if request.method == 'GET':    
+        return jsonify(data_json)
     elif request.method == 'POST':
         rd.flushdb()
         for item in data_json.keys():
             rd.set(item,json.dumps(data_json[item]))
-        return '---- Data Uploaded Successfully ----\n'
-
+        return '---- Data Uploaded Successfully to Redis ----\n'
 
 @app.route('/getInfo', methods=['GET'])
 def get_dataset_info():
     colKeys = []
     for key in rd.keys():
         colKeys.append(key.decode('utf-8'))
-    return json.dumps(list(set(colKeys)), indent=2) + '\n'
+    return f"Columns in Dataset:\n  {colKeys}\n"
 
 @app.route('/calcAvg/<col>', methods=['GET'])
 def calc_col_avg(col):
@@ -101,39 +100,33 @@ def calc_col_avg(col):
     for key in rd.keys():
         if key.decode('utf-8') == col:
             colList  = json.loads(rd.get(key).decode('utf-8'))
-            jobpayload = {'jobpayload': {
-                    'jobtype': 'calcAvg',
-                    'col': key.decode('utf-8')
-                         }
-            }
-            jobs.add_job(jobpayload, current_time(), "NA")
             for i in colList:
                 total = total + float(colList[i])
             avg = total/len(colList)
     return f'The average of {col} is {avg}\n'
 
+
 @app.route('/getInfo/column/<col>', methods=['GET'])
 def get_col_info(col):
-    colList  = []
+    colList  = {}
     for key in rd.keys():
         if key.decode('utf-8') == col:
             colList  = json.loads(rd.get(key).decode('utf-8'))
-    return json.dumps(list(set(colList)), indent=2) + '\n'
-
+    return f"{col} column in Dataset:\n  {colList}\n"
 
 @app.route('/getInfo/all', methods=['GET'])
 def get_all_info():
-    allList = {}
+    allList = []
     for key in rd.keys():
-        allList[key.decode('utf-8')]=json.loads(rd.get(key).decode('utf-8'))
-    return json.dumps(allList, indent=2) + '\n'
+        allList.append(rd.get(key))
+    return f"{list(allList)}\n"
 
 @app.route('/getInfo/row/<row>',methods=['GET'])
 def get_row_info(row):
     rowList = []
     for key in rd.keys():
         rowList.append(json.loads(rd.get(key).decode('utf-8')).get(row))
-    return json.dumps(list(set(rowList)), indent=2) + '\n'
+    return f"{list(rowList)}\n"
 
 @app.route('/getInfo/<col>/highest', methods=['GET'])
 def get_col_highest(col):
@@ -157,12 +150,13 @@ def get_col_lowest(col):
                      min  = float(colList.get(str(i)))
       return f"The lowest data in {col} values is {min}\n"
 
+
 @app.route('/getInfo/<row>/<col>', methods=['GET'])
 def get_data_value(row, col):
     for key in rd.keys():
         if key.decode('utf-8') == col:
-            locList  = json.loads(rd.get(key).decode('utf-8'))
-            return json.dumps(list(set(locList)), indent=2)
+            colList  = json.loads(rd.get(key).decode('utf-8'))
+            return f"The value in {row} row {col} column is {colList.get(row)}\n"
 
 @app.route('/getLoc/<col>/<value>', methods=['GET'])
 def get_value_position(col, value):
@@ -173,18 +167,11 @@ def get_value_position(col, value):
             for i in range(len(colList)):
                 if float(value) == colList.get(str(i)):
                     position.append(i)
-    return json.dumps(list(set(position)), indent=2)
+    return f"The position content {value} value are {list(position)}\n"
+
 
 @app.route('/calcVar/<col>', methods=['GET'])
 def cal_col_var(col):
-    '''
-    jobpayload = {'jobpayload': {
-                    'jobtype': 'calcVar',
-                    'column': col
-                    }
-                 }
-    jobs.add_job(jobpayload, current_time(), "NA")
-    '''
     colData = []
     for key in rd.keys():
         if key.decode('utf-8') == col:
@@ -194,8 +181,7 @@ def cal_col_var(col):
     return f'The variance of {col} is {statistics.variance(colData)}\n'
 
 @app.route('/create/<sr>/<rr>/<t>/<lm>/<bo>/<rem>/<sr1>/<hr>/<sl>', methods=['POST'])
-def create_new_row(sr:float,rr:float,t:float,lm:float,bo:float,rem:float,sr1:float,hr:flo\
-at,sl:float):
+def create_new_row(sr:float,rr:float,t:float,lm:float,bo:float,rem:float,sr1:float,hr:float,sl:float):
     """
     add a new row fo dataset to the redis
     """
@@ -229,62 +215,39 @@ def delete_data():
         rd.delete(key)
     return f'All data for in the dataset has been deleted.\n'
 
+@app.route('/graph/<columnOne>/<columnTwo>', methods=['GET'])
+def graphs(columnOne,columnTwo):
+    """
+    Creates a graph of data points based on the parameters provided.
+
+    -> Returns an image to the directory.
+    """
+    x_vals = []
+    y_vals1 = []
+    y_vals2 = []
+
+    for key in rd.keys():
+        i=0
+        if (key.decode('utf-8') == columnOne): 
+            colList  = json.loads(rd.get(key).decode('utf-8'))
+            for val in colList:
+                x_vals.append(i)
+                i=i+1
+                y_vals1.append(colList.get(val))
+
+        if (key.decode('utf-8') == columnTwo):
+            colList  = json.loads(rd.get(key).decode('utf-8'))
+            for val in colList:
+                y_vals2.append(colList.get(val))
+
+    plt.xlabel("Rows")
+    plt.title("Data comparison between "+columnOne+" and "+columnTwo)
+    plt.ylabel("Data Points")
+    plt.plot(x_vals, y_vals1, 'b--')
+    plt.plot(x_vals, y_vals2, 'b--')
+    plt.savefig('/output_image.png')
+    return("Graph output saved to local directory: '/output_image.png'\n")
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    
-'''
-ROUTE IDEAS
-
-/get<dataset>
--Retrieves specifc collumn of data for users can see. (all in column)
-
-/get<dataset><specific index>
--Retrieves a specifc dataset at index (column, row)
-
-/get<specific index>
--Retrieves whole row of data at specific index
-
-/get<dataset>highest
--Retrieves highest data point in a dataset column
-
-/get<dataset>lowest
--Retrieves lowest data point in dataset column
-
-/get<dataset>average
--Calculates average in dataset column
-
-/get<dataset>mean
--Gets most common dataset column value
-
-/getInfo
--Returns column names, sizes, types, and other information points about the dataset
-
-/set<location>
--Changes a data point at set location. (Checks for correct position, datatype matches entered type)
-
-/set<dataset>
--Takes an input of a data column (can be json or something else) and replaces the dataset with the new column. (Need to check for proper size, type and location)
-
-/set<user>
--Takes an input of user (or timestamp, whatever we're calling the rows) and replaces all data in that row. (Check size and data types match)
-
-/replaceAll<value><new value>
--Finds all instances of <value> and replaces them with <new value>
-
-/getRuntime [might not need this, could be cool tho]
--Returns Runtime for each job requested. 
-
-/getErrors
--Returns logging errors
-
-
-Should we have a system of plotting using matplotlib? It would look very nice
-for data analysis.
-(EX- /plot<dataColumn1><dataColumn2> -> returns a image graph)
-
-
-
-
-
-
-'''
