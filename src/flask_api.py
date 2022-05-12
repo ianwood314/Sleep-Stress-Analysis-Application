5600b206633ee1b165d9fed25f59d355b44c1628
Thy4869 <91298368+Thy4869@users.noreply.github.com>
2022-05-12 01:28:53 -0500

    Update flask_api.py

--- src/flask_api.py
+++ src/flask_api.py
@@ -2,32 +2,47 @@
 
 from flask import Flask, request, jsonify
 import pandas as pd
-import json, statistics
+import json, statistics, 
 from datetime import datetime
 from jobs import add_job, get_job_by_id
 
 app = Flask(__name__)
 redis_ip = os.environ.get('REDIS_IP')
 rd = redis.Redis(host=redis_ip, port=6379, db=0)
-q = hotqueue.HotQueue("queue", host=redis_ip, port=6379, db=1)
 jdb = redis.Redis(host=redis_ip, port=6379, db=2, decode_responses=True)
 
 def current_time():
     now = datetime.now()
     return now.strftime('%d/%m/%Y %H:%M:%S')
 
-@app.route('/jobs', methods=['POST'])
+@app.route('/jobs', methods=['POST', 'GET'])
 def jobs_api():
-    """
-    API route for creating a new job to do some analysis. This route accepts a JSON payload   
-    describing the job to be created.
-    """
-     
-    try:
-        job = request.get_json(force=True)
-    except Exception as e:
-        return True, json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
-    return json.dumps(add_job(job['start'], job['end']))
+      """
+      API route for creating a new job to do some analysis. This route accepts a JSON pay\
+load
+      describing the job to be created.
+      """
+      if request.method == 'POST':
+          try:
+              job = request.get_json(force=True)
+          except Exception as e:
+              return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format\
+(e)})
+          return json.dumps(add_job(job['row'], job['col'], job['start'], job['end']), in\
+dent=2) + '\n'
+
+      elif request.method == 'GET':
+          redis_dict = {}
+          for key in jdb.keys():
+              redis_dict[str(key)] = {}
+              redis_dict[str(key)]['datetime'] = jdb.hget(key, 'datetime')
+              redis_dict[str(key)]['status'] = jdb.hget(key, 'status')
+          return json.dumps(redis_dict, indent=4) + '\n' + """
+  To submit a job, do the following:
+  curl localhost:5034/jobs -X POST -d '{"jobtype":<job>, "variable":<key>}' -H "Content-T\
+ype: application/json"
+"""
+
 
 @app.route('/test', methods=['GET'])
 def hello_world():
@@ -49,24 +64,35 @@ a values\n"
 @app.route('/uploadData', methods=['GET','POST'])
 def upload_dataset():
     global data
-    data = pd.read_csv('./src/SaYoPillow-2.csv')
+    data = pd.read_csv('SaYoPillow-2.csv')
     data_json = data.to_json(orient='columns')
     data_json = json.loads(data_json)
-    
-    if request.method == 'GET':    
-        return jsonify(data_json)
+
+    if request.method == 'GET':
+        start = request.args.get('start', 0)
+        try:
+            start = int(start)
+            for key in rd.scan_iter():
+                d = rd.hgetall(key)
+                d = {key.decode('utf-8'): d[key].decode('utf-8') for key in d.keys()}
+                data.append(d)
+        except ValueError:
+            return ' -- Invalid start parameter. Please ensure the start parameter is an \
+integer --\n'
+        return f"{type(data_json)}\n"
     elif request.method == 'POST':
         rd.flushdb()
         for item in data_json.keys():
             rd.set(item,json.dumps(data_json[item]))
-        return '---- Data Uploaded Successfully to Redis ----\n'
+        return '---- Data Uploaded Successfully ----\n'
+
 
 @app.route('/getInfo', methods=['GET'])
 def get_dataset_info():
     colKeys = []
     for key in rd.keys():
         colKeys.append(key.decode('utf-8'))
-    return f"Columns in Dataset:\n  {colKeys}\n"
+    return json.dumps(list(set(colKeys)), indent=2) + '\n'
 
 @app.route('/calcAvg/<col>', methods=['GET'])
 def calc_col_avg(col):
@@ -77,37 +103,38 @@ def calc_col_avg(col):
             colList  = json.loads(rd.get(key).decode('utf-8'))
             jobpayload = {'jobpayload': {
                     'jobtype': 'calcAvg',
-                    'col': key
-                    }
-                 }
+                    'col': key.decode('utf-8')
+                         }
+            }
             jobs.add_job(jobpayload, current_time(), "NA")
             for i in colList:
                 total = total + float(i)
             avg = total/len(colList)
-    return f'The average of {col} is {avg}\n'
-
+    key = f'The average of {col} is {avg}\n'
+    return json.dumps(rd.hgetall(key), indent=2)
 
 @app.route('/getInfo/column/<col>', methods=['GET'])
 def get_col_info(col):
-    colList  = {}
+    colList  = []
     for key in rd.keys():
         if key.decode('utf-8') == col:
             colList  = json.loads(rd.get(key).decode('utf-8'))
-    return f"{col} column in Dataset:\n  {colList}\n"
+    return json.dumps(list(set(colList)), indent=2) + '\n'
+
 
 @app.route('/getInfo/all', methods=['GET'])
 def get_all_info():
-    allList = []
+    allList = {}
     for key in rd.keys():
-        allList.append(rd.get(key))
-    return f"{list(allList)}\n"
+        allList[key.decode('utf-8')]=json.loads(rd.get(key).decode('utf-8'))
+    return json.dumps(allList, indent=2) + '\n'
 
 @app.route('/getInfo/row/<row>',methods=['GET'])
 def get_row_info(row):
     rowList = []
     for key in rd.keys():
         rowList.append(json.loads(rd.get(key).decode('utf-8')).get(row))
-    return f"{list(rowList)}\n"
+    return json.dumps(list(set(rowList)), indent=2) + '\n'
 
 @app.route('/getInfo/<col>/highest', methods=['GET'])
 def get_col_highest(col):
@@ -118,7 +145,8 @@ def get_col_highest(col):
              for i in range(len(colList)):
                  if float(colList.get(str(i))) >= max:
                      max  = float(colList.get(str(i)))
-    return f"The highest data in {col} values is {max}\n"
+    key = "The highest data in {col} values is {max}\n"
+    return json.dumps(rd.hgetall(key), indent=2)
 
 @app.route('/getInfo/<col>/lowest', methods=['GET'])
 def get_col_lowest(col):
@@ -129,15 +157,17 @@ def get_col_lowest(col):
              for i in range(len(colList)):
                  if float(colList.get(str(i))) <= min:
                      min  = float(colList.get(str(i)))
-      return f"The lowest data in {col} values is {min}\n"
+      key = f"The lowest data in {col} values is {min}\n"
+      return json.dumps(rd.hgetall(key), indent=2)
+
 
 
 @app.route('/getInfo/<row>/<col>', methods=['GET'])
 def get_data_value(row, col):
     for key in rd.keys():
         if key.decode('utf-8') == col:
-            colList  = json.loads(rd.get(key).decode('utf-8'))
-            return f"The value in {row} row {col} column is {colList.get(row)}\n"
+            locList  = json.loads(rd.get(key).decode('utf-8'))
+            return json.dumps(list(set(locList)), indent=2)
 
 @app.route('/getLoc/<col>/<value>', methods=['GET'])
 def get_value_position(col, value):
@@ -148,8 +178,7 @@ def get_value_position(col, value):
             for i in range(len(colList)):
                 if float(value) == colList.get(str(i)):
                     position.append(i)
-    return f"The position content {value} value are {list(position)}\n"
-
+    return json.dumps(list(set(position)), indent=2)
 
 @app.route('/calcVar/<col>', methods=['GET'])
 def cal_col_var(col):
